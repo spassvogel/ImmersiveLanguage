472c83cc9007e0b413a0ceaa4b55517fd6fb169e
Remi <remialkemade@gmail.com>
2015-11-04 20:52:57 +0100

    Client inheriting from NetworkLobbyManager, sending messages like a boss.

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class Client : NetworkLobbyManager {
+
+    protected bool valid = true;
+
+    public const int MSG_GENERIC = 1001;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    override public void OnStartClient(NetworkClient client)
+    {
+        base.OnStartClient(client);
+        client.RegisterHandler(Client.MSG_GENERIC, onGenericMessage);
+    }
+
+    private void onGenericMessage(NetworkMessage netMsg)
+    {
+        VoipMessage voipMsg = netMsg.ReadMessage<VoipMessage>();
+        Debug.Log(voipMsg.message);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            CmdSendMessage("jupjup");
+        }
+    }
+
+    [Command]
+    void CmdSendMessage(string msg)
+    {
+        VoipMessage message = new VoipMessage();
+        message.message = msg;
+        NetworkServer.SendToAll(Client.MSG_GENERIC, message);
+    }
+}
