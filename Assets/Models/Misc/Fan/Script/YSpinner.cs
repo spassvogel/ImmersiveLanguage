dadf8e602d7d260f9c174156a65e92419f8a3154
spassvogel <exo_00@hotmail.com>
2015-11-12 15:23:51 +0100

    Added an animated fan

---
+++ Assets/Models/Misc/Fan/Script/YSpinner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YSpinner : MonoBehaviour {
+
+
+    [SerializeField]
+    private float speed = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * speed, 0);
+    }
+}
