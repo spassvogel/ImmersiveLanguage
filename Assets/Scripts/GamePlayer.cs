e2f40bd7808a7f881634c9b7ad858710e48e0e88
spassvogel <exo_00@hotmail.com>
2015-11-29 23:39:00 +0100

    Explored hook

--- Assets/Scripts/GamePlayer.cs
+++ Assets/Scripts/GamePlayer.cs
@@ -4,11 +4,15 @@ using System;
 using UnityEngine.Networking.NetworkSystem;
 using UnityEngine.Networking;
 
-public class ImmersiveNetworkLobbyPlayer : NetworkLobbyPlayer {
+public class GamePlayer : NetworkLobbyPlayer {
+
+    [SerializeField]
+    private AudioListener audioListener;    
 
     // Use this for initialization
     void Start () {
-	
+	   Debug.Log("A new GamePlayer is born! is local? "  + isLocalPlayer);
+       
 	}
 	
 	// Update is called once per frame
