1d59bd869488c63ae2282af67489c2864c436800
spassvogel <exo_00@hotmail.com>
2015-11-29 22:32:55 +0100

    Using new lobby

--- Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
+++ Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
@@ -3,16 +3,20 @@ using UnityStandardAssets.Network;
 using System.Collections;
 using UnityEngine.Networking;
 
+
 public class NetworkLobbyHook : LobbyHook
 {
     public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
     {
+        Debug.Log("the scene is loaded ");
+        
         LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
-        NetworkSpaceship spaceship = gamePlayer.GetComponent<NetworkSpaceship>();
-
+        //gamePlayer.transform.position = new Vector3(Random.Range(0, 3), 0, 0);
+        //NetworkSpaceship spaceship = gamePlayer.GetComponent<NetworkSpaceship>();
+/*
         spaceship.name = lobby.name;
         spaceship.color = lobby.playerColor;
         spaceship.score = 0;
-        spaceship.lifeCount = 3;
+        spaceship.lifeCount = 3;*/
     }
 }
