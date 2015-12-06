80ad7f74cb6d9139824f8eefa40da4e9cfc929d3
spassvogel <exo_00@hotmail.com>
2015-12-06 23:49:41 +0100

    Started to work on different player avatars. Made a prefab for the old guy

---
+++ Assets/Scripts/Face/SimpleEyeBlink.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleEyeBlink : MonoBehaviour {
+
+	[SerializeField]	private Transform leftEyelidUp;
+	[SerializeField]	private Transform leftEyelidDown;
+	[SerializeField]	private Transform rightEyelidUp;
+	[SerializeField]	private Transform rightEyelidDown;
+	
+	[SerializeField]	private Vector3 openRotationUp;
+	[SerializeField]	private Vector3 closedRotationUp;
+	[SerializeField]	private Vector3 openRotationDown;
+	[SerializeField]	private Vector3 closedRotationDown;
+
+    [Tooltip("Time in seconds that the eyes will be closed during a blink")]
+	[SerializeField]	private float blinkDuration = .1f;	
+	
+    [Tooltip("Minimum frequency of blink")]
+	[SerializeField]	private float minBlinkDelay = 2;
+    [Tooltip("Maximum frequency of blink")]
+	[SerializeField]	private float maxBlinkDelay = 4;
+	
+	private bool eyesClosed = true;
+	private float nextBlink;
+
+	// Use this for initialization
+	void Start () {
+		OpenEyes();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextBlink){
+			eyesClosed = true;
+			Invoke("OpenEyes", blinkDuration);
+		}
+		
+		if(eyesClosed){
+			leftEyelidDown.transform.localRotation = Quaternion.Euler(closedRotationDown);
+			rightEyelidDown.transform.localRotation = Quaternion.Euler(closedRotationDown);
+			leftEyelidUp.transform.localRotation = Quaternion.Euler(closedRotationUp);
+			rightEyelidUp.transform.localRotation = Quaternion.Euler(closedRotationUp);
+		}
+		else {
+			leftEyelidDown.transform.localRotation = Quaternion.Euler(openRotationDown);
+			rightEyelidDown.transform.localRotation = Quaternion.Euler(openRotationDown);
+			leftEyelidUp.transform.localRotation = Quaternion.Euler(openRotationUp);
+			rightEyelidUp.transform.localRotation = Quaternion.Euler(openRotationUp);			
+		}
+	}
+	
+	private void OpenEyes(){
+		nextBlink = Time.time + Random.Range(minBlinkDelay, maxBlinkDelay);
+		eyesClosed = false;
+	}
+}
