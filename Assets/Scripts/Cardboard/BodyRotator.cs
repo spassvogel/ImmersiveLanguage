f27d75b7fb97dc7ad509d12f8ba478ee9663fd2a
spassvogel <exo_00@hotmail.com>
2015-12-27 00:34:58 +0100

    Scripts were not added in previous commit

---
+++ Assets/Scripts/Cardboard/BodyRotator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+/* 
+This rotates the current */
+public class BodyRotator : MonoBehaviour {
+
+[SerializeField]
+private Transform head;
+[SerializeField]
+private float speed = .02f;
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 rootRotation = transform.rotation.eulerAngles;
+		Vector3	headRotation = head.rotation.eulerAngles;
+				
+		headRotation.x = rootRotation.x;
+		headRotation.z = rootRotation.z;		
+				
+		float yRotation = Vector3.Slerp(rootRotation, headRotation, speed * Time.deltaTime).y;
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(headRotation) , speed * Time.deltaTime * Quaternion.Angle(transform.rotation, head.rotation));
+	}
+	
+}
