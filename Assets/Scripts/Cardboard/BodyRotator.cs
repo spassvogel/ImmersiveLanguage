a85287d0e3119cd8d7fec6ff4ebfc82aa79cb0da
Remi <remialkemade@gmail.com>
2015-12-30 21:05:34 +0100

    Fixed body rotation based on head rotation.

--- Assets/Scripts/Cardboard/BodyRotator.cs
+++ Assets/Scripts/Cardboard/BodyRotator.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
-/* 
-This rotates the current */
+
+// BodyRotator rotates its transform by the difference between the y-angle of the given Head and Hips,
+// with a slack defined by maxAngle. 
 public class BodyRotator : MonoBehaviour {
 
-[SerializeField]
-private Transform head;
-[SerializeField]
-private float speed = .02f;
-	// Update is called once per frame
+    [SerializeField]
+    private Transform head;
+
+    [SerializeField]
+    private Transform hips;
+
+    [SerializeField]
+    private float maxAngle = 20f;
+
 	void Update () 
 	{
-		Vector3 rootRotation = transform.rotation.eulerAngles;
+		Vector3 hipRotation = hips.rotation.eulerAngles;
 		Vector3	headRotation = head.rotation.eulerAngles;
-				
-		headRotation.x = rootRotation.x;
-		headRotation.z = rootRotation.z;		
-				
-		float yRotation = Vector3.Slerp(rootRotation, headRotation, speed * Time.deltaTime).y;
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(headRotation) , speed * Time.deltaTime * Quaternion.Angle(transform.rotation, head.rotation));
+       
+        float yAngle = Mathf.DeltaAngle(headRotation.y, hipRotation.y);
+       
+        if(Mathf.Abs(yAngle) > maxAngle) {
+            float rotate;
+            if(yAngle < 0) {
+                rotate = -1 * yAngle - maxAngle;
+            } else {
+                rotate = maxAngle - yAngle;
+            }
+            transform.Rotate(0f, rotate, 0f);
+        }
 	}
 	
 }
